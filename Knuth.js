4ee37b410eca82bdb6135b3c79e3612921a753b3
Bo <bo@Bos-MacBook-Pro.lan>
2017-04-14 14:38:27 -0700

    Knuth

--- Knuth.js
+++ Knuth.js
@@ -1,13 +1,54 @@
+function visualizeKnuth(tree, SIZE) {
+  knuth_layout(tree, SIZE);
+  var svgContainer = d3.select("body")
+                         .append("svg")
+                         .attr("width", SIZE * 100)
+                         .attr("height", SIZE * 100)
+                         .style("border", "1px solid black");
+  function solve(node) {
+    console.log(node.x);
+    console.log(node.y);
+    svgContainer.append("circle")
+        .attr("cx", node.x )
+        .attr("cy", node.y )
+        .attr("r", SIZE / 4)
+        .style("fill", "purple");
+    if (node.left !== null){
+      svgContainer.append("line")
+                  .attr("x1", node.x)
+                  .attr("y1", node.y)
+                  .attr("x2", node.left.x)
+                  .attr("y2", node.left.y)
+                  .attr("stroke-width", 2)
+                  .attr("stroke", "purple");
+      solve(node.left);
+    }
 
+    if (node.right !== null){
+      svgContainer.append("line")
+                  .attr("x1", node.x)
+                  .attr("y1", node.y)
+                  .attr("x2", node.right.x)
+                  .attr("y2", node.right.y)
+                  .attr("stroke-width", 2)
+                  .attr("stroke", "purple");;
+      solve(node.right);
+    }
 
-function knuth_layout(Tree){
+  }
+  solve(tree);
+}
+
+function knuth_layout(Tree, SIZE) {
   var i = 0;
-  function knuth_layout_solve(node, depth){
-    if(node.left !== null) knuth_layout_solve(node.left, depth + 1);
+  function knuth_layout_solve(node, depth) {
+    if (node.left !== null)
+      knuth_layout_solve(node.left, depth + 1);
     node.x = i;
-    node.y = depth;
-    i+=1;
-    if(node.right !== null) knuth_layout_solve(node.right, depth + 1);
+    node.y = depth * SIZE;
+    i += SIZE;//
+    if (node.right !== null)
+      knuth_layout_solve(node.right, depth + 1);
   }
   knuth_layout_solve(Tree, 0);
 }
--- Tree.js
+++ Tree.js
@@ -1,42 +1,45 @@
 /*
 format of data
 
-index parentIndex
-index parentIndex
+[[index, parentIndex],[index, parentIndex],[index, parentIndex],...]
 
-Note: left child appears first than right child
+parentIndex of root is -1
+
+Note: the node appears first will be treated as left child 
 */
 
-function Tree(data){
-  //maintain  a temperary table to restore index-node map
+function Tree(data) {
+  // maintain  a temperary table to restore index-node map
   var table = {};
-  //make Node
-  for(var i = 0; i < data.length; i++){
+  // make Node
+  for (var i = 0; i < data.length; i++) {
 
     index = data[i][0];
     node = new Node(index);
     table[index] = node;
   }
-  //construte tree
-  for(var i = 0; i < data.length; i++){
+  // construte tree
+  for (var i = 0; i < data.length; i++) {
     parent = table[data[i][1]];
     child = table[data[i][0]];
-    if(parent != undefined){
-      if(parent.left === null) parent.left = child;
-      else if(parent.right === null) parent.right = child;
-      else throw "invalid tree. Please enter binary tree";
-
+    if (parent != undefined) {
+      if (parent.left === null)
+        parent.left = child;
+      else if (parent.right === null)
+        parent.right = child;
+      else
+        throw "invalid tree. Please enter binary tree";
     }
   }
-  //return root
-  for(var i = 0; i < data.length;i++){
-    if(data[i][1] == -1){
+  // return root
+  for (var i = 0; i < data.length; i++) {
+    if (data[i][1] == -1) {
       return table[data[i][0]];
     }
   }
 }
 
-function Node(index, left = null, right = null, x = 0, y = 0){
+function Node(index, left = null, right = null, x = 0, y = 0) {
   this.index = index;
   this.left = left;
   this.right = right;
